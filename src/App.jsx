2a60d96fafb1c6af238f10f091c7c4c5ad03a77f
Godamaishwarya <godamaishwarya39@gmail.com>
2024-11-07 19:35:11 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+    <h1 style={{backgroundColor:'red'}}>Hello</h1>
+    <div style={{fontSize:50}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis quas beatae fuga dolore quis ipsa ipsum hic, consectetur distinctio eum.</div>
+    </>
+  )
+}
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// createRoot(document.getElementById('root')).render(
+//     <App />
+// )     //main cha code always asa pahije
+
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+// let a = 'mine'
+createRoot(document.getElementById('root')).render(
+    <App />
+)
